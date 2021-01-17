80a9c407b175575440d499486f57ba3eb252bdfc
madisapsalon <madis.apsalon@gmail.com>
2021-01-17 20:49:59 +0200

    Add role and user models, repositories

---
+++ src/main/java/com/training/api/entity/role/Role.java
@@ -0,0 +1,21 @@
+package com.training.api.entity.role;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+}
