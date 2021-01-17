0a099443071c110b8695af64c9cde2e7b7a18425
madisapsalon <madis.apsalon@gmail.com>
2021-01-17 21:20:16 +0200

    Add preauthorizations for ExerciseCategoryController

--- src/main/java/com/training/api/controller/ExerciseCategoryController.java
+++ src/main/java/com/training/api/controller/ExerciseCategoryController.java
@@ -6,6 +6,7 @@ import com.training.api.service.ExerciseCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +24,7 @@ public class ExerciseCategoryController {
     }
 
     @PostMapping()
+    @PreAuthorize("hasRole('ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<ExerciseCategory> create(@RequestBody ExerciseCategory category) {
         ExerciseCategory newCategory = this.exerciseCategoryService.createExerciseCategory(category);
@@ -31,12 +33,14 @@ public class ExerciseCategoryController {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<CategoryDto> getById(@PathVariable Integer id) {
         CategoryDto category = this.exerciseCategoryService.getById(id);
         return ResponseEntity.ok(category);
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<List<CategoryDto>> getAll() {
         List<CategoryDto> types = this.exerciseCategoryService.getAllCategories();
         return ResponseEntity.ok(types);
